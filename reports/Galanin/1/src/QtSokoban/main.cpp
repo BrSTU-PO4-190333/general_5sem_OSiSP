fd0023bedc667e47d38475ea0d30224ecd1fe722
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-09-22 21:45:06 +0300

    lab1/Galanin_Pavel : Updated UI

--- reports/Galanin/1/src/QtSokoban/main.cpp
+++ reports/Galanin/1/src/QtSokoban/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowIcon(QIcon(":/img/_pics/favicon.png"));
+    w.setWindowTitle("Sokoban");
     w.show();
     return a.exec();
 }
