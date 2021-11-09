096e733a9b30e3a681f21af379cd07a4347e3f97
Gritsak Alexandra <74914400+gritsak-alexandra@users.noreply.github.com>
2021-11-10 00:14:12 +0300

    Create helper.h

---
+++ reports/Gritsak_Alexandra/lab3/src/helper.h
@@ -0,0 +1,11 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include "helper_global.h"
+#include "QString"
+
+extern "C" HELPER_EXPORT QString GetStart();
+extern "C" HELPER_EXPORT QString Start();
+extern "C" HELPER_EXPORT QString Exit();
+
+#endif // HELPER_H
