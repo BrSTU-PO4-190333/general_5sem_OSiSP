addc81af2da70726b3a07bafd1c66008c549460c
Gritsak Alexandra <74914400+gritsak-alexandra@users.noreply.github.com>
2021-11-10 00:16:47 +0300

    Add files via upload

---
+++ reports/Gritsak_Alexandra/lab3/src/about.h
@@ -0,0 +1,8 @@
+#ifndef ABOUT_H
+#define ABOUT_H
+
+#include "about_global.h"
+
+extern "C" ABOUT_EXPORT void About();
+
+#endif // ABOUT_H
---
+++ reports/Gritsak_Alexandra/lab3/src/startwindow.h
@@ -0,0 +1,29 @@
+#ifndef STARTWINDOW_H
+#define STARTWINDOW_H
+
+#include <QDialog>
+#include <QtWidgets>
+#include <../../helper/helper.h>
+
+
+
+class StartWindow : public QDialog
+{
+public:
+     StartWindow(QWidget *parent = 0);
+    ~StartWindow();
+
+    QRadioButton *newg;
+    QPushButton *start, *exit;
+    QVBoxLayout *startgame, *full;
+    QHBoxLayout *buttons;
+
+    QIcon windowIcon;
+
+public slots:
+    void unlockingButton();
+    int clicked_start();
+    bool clicked_exit();
+};
+
+#endif // STARTWINDOW_H
